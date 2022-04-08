327d95a2745cfbf9445af88fad7e018bbd90f37f
Marcos <marcintosh87@gmail.com>
2022-04-08 17:15:48 -0400

    resolved an issue with npm and expo cli

--- spiritly-mobile/App.js
+++ spiritly-mobile/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Box, NativeBaseProvider, TextArea } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
+import BottomNav from "./components/BottomNav";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Box>
+          <Text fontSize="lg">lg</Text>
+        </Box>
+
+        <BottomNav />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
