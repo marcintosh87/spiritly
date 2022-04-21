c60b91ea832c3dce052aa5229fa5f3a20936217d
Marcos <marcintosh87@gmail.com>
2022-04-21 13:22:08 -0400

    added react native navigation bottom bar skeleton

--- spiritly-mobile/App.js
+++ spiritly-mobile/App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { Box, NativeBaseProvider, TextArea } from "native-base";
-import { StyleSheet, Text, View } from "react-native";
-import BottomNav from "./components/BottomNav";
+import { Box, NativeBaseProvider, TextArea, Text } from "native-base";
+import { StyleSheet, View } from "react-native";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./components/Home";
+import Home2 from "./components/Settings";
+import Settings from "./components/Settings";
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Box>
-          <Text fontSize="lg">lg</Text>
-        </Box>
-
-        <BottomNav />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{ tabBarShowLabel: false, headerShown: false }}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
@@ -21,6 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
---
+++ spiritly-mobile/components/Settings.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Settings() {
+  return (
+    <View style={styles.container}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+});
